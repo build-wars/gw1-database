3be36bcc20f883dcf206819195e9853967f7f31c
smiley <smiley@chillerlan.net>
2015-11-20 15:00:44 +0100

    hello :octocat:

---
+++ src/Database/Connectors/GWDBConnectorInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Interface GWDBConnectorInterface
+ *
+ * @filesource   GWDBConnectorInterface.php
+ * @created      06.11.2015
+ * @package      chillerlan\GW1Database\Database\Connectors
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\GW1Database\Database\Connectors;
+
+/**
+ * Do whatever you need to do to connect to your framework's database, just implement this interface.
+ *
+ * Note: the GWBBParserOptions object is being passed to the constructor,
+ * use it's $connector_options property to pass data (or an existing database connection) along
+ *
+ * @see \chillerlan\GW1Database\Database\Connectors\ChillerlanFramework
+ */
+interface GWDBConnectorInterface{
+
+
+}
