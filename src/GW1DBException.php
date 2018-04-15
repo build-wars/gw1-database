35b7b96a8f4d3022902d10cf465e0c7a0bfb6086
codemasher <smiley@chillerlan.net>
2018-04-15 22:10:41 +0200

    :sparkles:

---
+++ src/GW1DBException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class GW1DBException
+ *
+ * @filesource   GW1DBException.php
+ * @created      12.04.2018
+ * @package      chillerlan\GW1DB
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2018 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\GW1DB;
+
+class GW1DBException extends \Exception{}
